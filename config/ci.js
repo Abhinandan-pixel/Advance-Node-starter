460a7a583953342a460e15054b7ea0c3fda8b7dc
Abhinandan-pixel <abhinandanmysore@gmail.com>
2024-05-14 21:53:13 +0530

    updated redis redis url

--- config/ci.js
+++ config/ci.js
@@ -4,6 +4,6 @@ module.exports = {
     googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
     mongoURI:"mongodb://localhost:27017/blog_ci",
     cookieKey: "123123123",
-    redisUrl: "redis://127.0.0.1:6379"
+    redisUrl: "redis://localhost:6379"
 };
   
